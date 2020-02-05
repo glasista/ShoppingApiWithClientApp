084cb3bd1c6168a2e896f74e37334f995dada823
glasista <ruszala.rafal@gmail.com>
2020-02-05 22:27:58 +0100

    Added Model and DbContext

---
+++ ShoppingApi/Models/ShoppingContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingApi.Models
+{
+    public class ShoppingContext : DbContext
+    {
+        public ShoppingContext(DbContextOptions<ShoppingContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ShoppingItem> ShoppingItems { get; set; }
+    }
+}
---
+++ ShoppingApi/Models/ShoppingItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ShoppingApi.Models
+{
+    public class ShoppingItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsTaken { get; set; }
+    }
+}
